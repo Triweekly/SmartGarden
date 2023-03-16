450bee2a44046d212d29baf937f7e982068eb368
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-03-16 16:08:01 -0400

    benchmarks for debugging

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -13,12 +13,14 @@ let url = urlBase + "/loadReadings." + extension;
     try
     {
         console.log(xhr.responseText);
+        console.log("logged response text, now will try to parse:");
         let jsonObject = JSON.parse(xhr.responseText);
         console.log(jsonObject);
         //in future, assign these to places on a table
     }
     catch(err)
     {
+        console.log("Error encountered:");
         console.log(err);
     }
 }
