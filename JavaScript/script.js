14a1726091e4846702c85561c657f7b7349474dc
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-03-16 20:52:24 -0400

    sync

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,7 +9,7 @@ let url = urlBase + "loadReadings." + extension;
 console.log(url);
 
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);    //FIXME trying Get instead of POST
+    xhr.open("GET", url, false);    //FIXME trying Get instead of POST
     
     // xhr.setRequestHeader("Content-type", "applciation/json; charset=UTF-8");    //disabling prevents errors, but doesn't make content received.
     
