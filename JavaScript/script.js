141b79a56772d7d1202b032dd725ef70baeb6534
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-03-16 20:01:16 -0400

    removing a header declaration from js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,7 +10,7 @@ console.log(url);
 
     let xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);    //FIXME trying Get instead of POST
-    xhr.setRequestHeader("Content-type", "applciation/json; charset=UTF-8");
+    // xhr.setRequestHeader("Content-type", "applciation/json; charset=UTF-8");
     xhr.send();
     try
     {
