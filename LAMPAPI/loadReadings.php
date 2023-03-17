9f3d3a04acae2d021bcc90dc8076739a7f9e5222
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-03-16 22:10:41 -0400

    changed read-from database

--- LAMPAPI/loadReadings.php
+++ LAMPAPI/loadReadings.php
@@ -17,7 +17,7 @@ if ($con->connect_error)
 	} 
 else
 {
-    $stmt = $con->prepare("SELECT * FROM currentplant");
+    $stmt = $con->prepare("SELECT * FROM currentreadings");
 	$stmt->execute();
 	$result = $stmt->get_result();
 	
