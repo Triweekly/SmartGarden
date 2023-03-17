b79c57deace0e0974c2307d18c52ab93bb26d317
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-03-16 21:29:06 -0400

    table test

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,12 @@
 const urlBase = 'http://www.smartgarden34.com/SmartGarden/LAMPAPI/';
 const extension = 'php';
 
+let currentName;
+let currentMoisture;
+let currentUV;
+let currentHumidity;
+let currentTemperature;
+
 function loadReadings()
 {
     console.log("Trying to get sensor readings...");
@@ -18,6 +24,10 @@ console.log(url);
     {
         console.log(xhr.response);  //responseText?
         console.log("logged response text (might be an empty line), now will try to parse:");
+        parseReadings(xhr.response);
+
+
+
         // let jsonObject = JSON.parse(xhr.responseText);
         // console.log("Next print SHOULD be the returned object from http://www.smartgarden34.com/SmartGarden/LAMPAPI/loadReadings.php");
         // console.log(jsonObject);
@@ -30,3 +40,48 @@ console.log(url);
     }
 }
 
+
+function parseReadings(inString)
+{
+    let temp = "";
+    let caseVar = 1;
+    for(let i = 0 ; i < inString.length ; i++)
+        {
+            if(inString.charAt(i)!=',')
+                {
+                    temp += inString.charAt(i);
+                }
+            else
+                {
+                    switch(caseVar) {
+                        case 1:
+                            currentName = temp;
+                            break;
+                        case 2:
+                            currentMoisture = temp;
+                            break;
+                        case 3:
+                            currentUV = temp;
+                            break;
+                        case 4:
+                            currentHumidity = temp;
+                            break;
+                        case 5:
+                            currentTemperature = temp;
+                            break;
+                    }
+                    temp = "";
+                    caseVar++;
+
+
+                }   
+
+        }   
+}
+
+//getters for table
+function getName(){return currentName;}
+function getMoisture(){return currentMoisture;}
+function getUV(){return currentUV;}
+function getHumidity(){return currentHumidity;}
+function getTemperature(){return currentTemperature;}
