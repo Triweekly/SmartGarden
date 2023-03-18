95c02d7778d5eee20f5b46bbb886aa5a7e286ac9
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-03-18 16:33:45 -0400

    new file for arduino to check in the future.

---
+++ LAMPAPI/checkPrefs.php
@@ -0,0 +1,9 @@
+<?php
+
+print("this page/button currently doesn't do anything. Will eventually provide the data to the arduino of what plant and preferences are currently being requested.");
+
+//get name from entry with all -1s
+//get stats from entry named Custom.
+//send as response (maybe as JSON? Maybe text the Arduino can parse more easily) like loadReadings.php
+
+?> 
