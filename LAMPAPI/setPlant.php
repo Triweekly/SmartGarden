5bfb39a959ee40446b47a8f7f4f7e68d096dfda3
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-03-20 12:58:35 -0400

    need to change name in currentreadings

--- LAMPAPI/setPlant.php
+++ LAMPAPI/setPlant.php
@@ -26,6 +26,10 @@ else
     $stmt = $con->prepare("UPDATE currentplant SET currentplant.name = ?, currentplant.moisture = (select plantprefs.prefmoisture from plantprefs where plantprefs.name = ?), currentplant.uv = (select plantprefs.prefuv from plantprefs where plantprefs.name = ?), currentplant.humidity = (select plantprefs.prefhumidity from plantprefs where plantprefs.name = ?), currentplant.temperature = (select plantprefs.preftemperature from plantprefs where plantprefs.name = ?)");	//FIXME? deleted a semicolon
     $stmt->bind_param('sssss', $name, $name, $name, $name, $name);	//d is float. string would be s, integers would be i
 	$stmt->execute();
+
+	//$stmt = update name in currentReadings //FIXME!!
+
+
 	$stmt->close();
     $con->close();
 	
