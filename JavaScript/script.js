5f8a9a61cc0ebc1dd816be28ab609eb8c5780bc5
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-04-02 18:45:36 -0400

    true==sync?async?

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -286,7 +286,7 @@ function addDroption()
     let url = urlBase + "getPlantNames." + extension;
 
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, false);
+    xhr.open("GET", url, true);
    
     xhr.send();
 
