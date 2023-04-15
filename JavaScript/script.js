a7cb14f6ebbdd3b8f441de4015ee9cb8f112af44
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-04-15 13:34:18 -0400

    true on droption

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -395,7 +395,7 @@ function addDroption()
     let url = urlBase + "getPlantNames." + extension;
 
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, false);
+    xhr.open("GET", url, true);
    
     xhr.send();
 
