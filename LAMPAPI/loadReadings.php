40257aa2b52a343048f7eb250e37bf15b91c8a8e
DieifGreen <97707464+DieifGreen@users.noreply.github.com>
2023-04-15 16:26:00 -0400

    Revert "Centered table"
    This reverts commit 760c6f865aa4d34c0312cdf3ea472a71745dc1d8.

--- LAMPAPI/loadReadings.php
+++ LAMPAPI/loadReadings.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://smartgarden34.com');
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
